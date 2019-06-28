0b2b9269d0f20c4362b9b5a6cfd11bd59ad46d6e
Deepwarrior <piskun_ya@mail.ru>
2019-06-28 17:48:42 +0300

    Add bot class

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot
+
+class Bot:
+    def __init__(self, token):
+        bot = telebot.TeleBot(token)
+
+    def give_link(self, chat, player):
+        pass
+
+    def register(self, player):
+        pass
+
+    def kusb(self, chat, player):
+        pass
+
+    def parse_text(self, chat):
+        pass
+
+    def check_answer(self, message):
+        pass
+
+    def end_turn(self):
+        pass
+
+    def change_info(self):
+        pass
